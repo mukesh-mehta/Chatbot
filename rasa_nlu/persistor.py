6f2ca9b834686ac0f7dc178554050de1f66ec6b0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 11:42:26 +0000

    Adding tarfile member sanitization to extractall()

--- rasa_nlu/persistor.py
+++ rasa_nlu/persistor.py
@@ -79,7 +79,26 @@ class AWSPersistor(Persistor):
         with io.open(filename, 'wb') as f:
             self.bucket.download_fileobj(filename, f)
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall(self.data_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self.data_dir)
 
 
 class GCSPersistor(Persistor):
@@ -120,4 +139,23 @@ class GCSPersistor(Persistor):
         blob.download_to_filename(filename)
 
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall(self.data_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, self.data_dir)
